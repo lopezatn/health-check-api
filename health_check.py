8f0b0120e3cd28a51af4b8056cdce6d3f02d82bb
lopezatn <agustinlpzb@gmail.com>
2026-03-16 18:24:46 +0000

    health-API call

--- health_check.py
+++ health_check.py
@@ -1,16 +1,22 @@
+from flask import Flask, request
 import sys, subprocess
 
-try:
-    container = sys.argv[1]
-except IndexError:
-    print("Missing arguments")
-    exit(1)
+app = Flask(__name__)
 
-docker_result = subprocess.check_output(f"docker ps --filter name={container}", shell=True, text=True)
+@app.route("/container-health")
+def health_check():
 
-if ("Up".casefold() in docker_result.casefold()):
-    print("The container is running")
-else:
-    print("The container is NOT running")
+    try:
+        container = request.args.get("container")
+    except IndexError:
+        return("Missing arguments")
 
+    docker_result = subprocess.check_output(f"docker ps --filter name={container}", shell=True, text=True)
 
+    if ("Up".casefold() in docker_result.casefold()):
+        return("The container is running")
+    else:
+        return("The container is NOT running")
+
+if __name__ == "__main__":
+    app.run()
